8d0b966c5ca7611b94da2a343774a35f6d8d20a6
jonathan.santos <jonathan.santos@sequor.com.br>
2020-09-10 17:13:44 -0300

    feat: add event on click on books list.

--- src/Components/ImageList.tsx
+++ src/Components/ImageList.tsx
@@ -1,32 +1,42 @@
 /* eslint-disable prettier/prettier */
 import { FlatList, TouchableOpacity, View, ImageBackground, Text, StyleSheet } from 'react-native';
 import React from 'react';
+import { accessibilityProps } from 'react-native-paper/lib/typescript/src/components/MaterialCommunityIcon';
 
 interface IProps{
     data: IData[];
+    onPress: any;
 }
 
 interface IData{
     title: string;
     imageUri: string;
+    data: any;
 }
 
-const renderItem = (item: IData) => {
-    return (
-        <TouchableOpacity onPress={() => console.warn("A")} >
-            <View style={styles.container}>
-                <View style={styles.imageContainer}>
-                    <ImageBackground style={styles.imageSize} source={ { uri: item.imageUri }} />
-                </View>
-                <View style={styles.titleContainer}>
-                    <Text>{item.title}</Text>
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
-};
+
 
 const ImageList: React.FC<IProps> = (props) => {
+
+    const renderItem = (item: IData) => {
+        return (
+            <TouchableOpacity onPress={() => {
+                    if (props.onPress){
+                        props.onPress(item.data);
+                    }
+                }} >
+                <View style={styles.container}>
+                    <View style={styles.imageContainer}>
+                        <ImageBackground style={styles.imageSize} source={ { uri: item.imageUri }} />
+                    </View>
+                    <View style={styles.titleContainer}>
+                        <Text>{item.title}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
         <FlatList
             data={props.data}
