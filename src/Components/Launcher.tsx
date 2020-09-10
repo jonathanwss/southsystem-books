32428709bcae89bc93d70d82770a96570472b0cc
jonathan.santos <jonathan.santos@sequor.com.br>
2020-09-10 12:00:37 -0300

    feat: remove unused components.

--- src/Components/Launcher.tsx
+++ src/Components/Launcher.tsx
@@ -2,21 +2,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { Text, View } from 'react-native';
-
-const test: React.FC<IProps> = ({ navigation }) => {
-    return (
-        <View>
-            <Text onPress={() => navigation.navigate('test')} style={{ color: 'black', fontSize: 50 }}>Test</Text>
-        </View> 
-    );
-};
-
-const test2: React.FC<IProps> = () => {
-    return (
-        <Text style={{ color: 'black' }}>Test 2</Text>
-    );
-};
+import ViewBooks from '../View/ViewBooks';
 
 interface IProps {}
 const Launcher:React.FC<IProps> =  () => {
@@ -25,8 +11,7 @@ const Launcher:React.FC<IProps> =  () => {
     return (
         <NavigationContainer>
             <stackNavigator.Navigator initialRouteName="home">
-                <stackNavigator.Screen name="home" component={test} />
-                <stackNavigator.Screen name="test" component={test2} />
+                <stackNavigator.Screen name="Books" component={ViewBooks} />
             </stackNavigator.Navigator>
         </NavigationContainer>
     );
