f5664d45c7b66fa64ebf350d563d14448b1efe3b
jonathan.santos <jonathan.santos@sequor.com.br>
2020-09-13 23:45:24 -0300

    feat: add test file.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import ImageList from '../src/Components/ImageList';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders ImageList', () => {
+  renderer.create(<ImageList />);
 });
